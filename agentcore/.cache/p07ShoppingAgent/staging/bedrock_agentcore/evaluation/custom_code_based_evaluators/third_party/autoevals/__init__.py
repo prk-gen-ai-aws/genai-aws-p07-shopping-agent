8bfc75a9a9cdb556c68991ef6341a46507ab2883
Pichaimani Rajesh Kumar <rkumarus040599@gmail.com>
2026-08-31 06:45:55 -0500

    p07: remove .cache from tracking, add to gitignore

--- agentcore/.cache/p07ShoppingAgent/staging/bedrock_agentcore/evaluation/custom_code_based_evaluators/third_party/autoevals/__init__.py
+++
@@ -1,5 +0,0 @@
-"""AutoEvals adapter for AgentCore code-based evaluators."""
-
-from bedrock_agentcore.evaluation.custom_code_based_evaluators.third_party.autoevals.adapter import AutoEvalsAdapter
-
-__all__ = ["AutoEvalsAdapter"]
--- agentcore/.cache/p07ShoppingAgent/staging/bedrock_agentcore/evaluation/custom_code_based_evaluators/third_party/deepeval/__init__.py
+++
@@ -1,5 +0,0 @@
-"""DeepEval adapter for AgentCore code-based evaluators."""
-
-from bedrock_agentcore.evaluation.custom_code_based_evaluators.third_party.deepeval.adapter import DeepEvalAdapter
-
-__all__ = ["DeepEvalAdapter"]
